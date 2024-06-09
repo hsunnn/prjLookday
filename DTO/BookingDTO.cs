7885526e96e21777815ccadf022013d34debcf05
2024kang <166598158+2024kang@users.noreply.github.com>
2024-06-09 09:42:33 +0800

    訂單管理新增

---
+++ DTO/BookingDTO.cs
@@ -0,0 +1,34 @@
+﻿namespace prjLookdayOrder.DTO
+{
+    public class BookingDTO
+    {
+        //很多參數打包再一起叫做DTO(Data Transfer Object)
+        //WEB API 沒有 VIEW 所以寫成DTO 取代 VIEW MODEL 　
+
+        public int BookingId { get; set; }
+        //允許DATATIME可以是 null。也允許該屬性值為空
+        public DateTime? BookingDate { get; set; } 
+        public decimal Price { get; set; }
+
+        public int UserID { get; set; }
+
+        //这行代码的含义是将 ActivityName 属性初始化为 null，但同时使用 !
+        //操作符表示即使它被初始化为 null，也表明在运行时不会出现空引用异常。
+        // public string UserName { get; set; } = null!;
+        public string? UserName { get; set; }
+
+        public int ActivityID { get; set; }
+       // public int ActivityID { get; set; }
+        //public string ActivityName { get; set; } = null!;
+        public string? ActivityName { get; set; } 
+        public string? bookingStatus { get; set; }
+        public int? member { get; set; }
+
+
+        //public User User { get; set; }
+        //public Activity Activity { get; set; }
+        //public BookingState BookingState { get; set; }
+        ////// public DateTime? PaymentDate { get; set; }
+        //////paymentmethod 也要寫
+    }
+}
---
+++ DTO/BookingFilterDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace prjLookdayOrder.DTO
+{
+    public class BookingFilterDTO
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
