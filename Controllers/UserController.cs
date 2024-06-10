5f19f16dc833af367f6111206109503a17f50c27
Hsun <pp0932451127@gmail.com>
2024-06-11 00:09:40 +0800

    UserList 新增筆數顯示設定 要改頁籤

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,31 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using prjLookday.Models;
 using prjLookday.ViewModels;
-using System.Security.Permissions;
+using System.Linq;
+using X.PagedList;
 
 namespace prjLookday.Controllers
 {
     public class UserController : SuperController
     {
-        private IWebHostEnvironment _enviro = null;
+        private readonly IWebHostEnvironment _enviro;
 
         public UserController(IWebHostEnvironment enviro)
         {
             _enviro = enviro;
         }
 
-        public IActionResult List(CKeywordViewModel vm)
+        public IActionResult List(CKeywordViewModel vm, int? page)
         {
             lookdaysContext db = new lookdaysContext();
 
-            IEnumerable<User> datas = null;
+            IEnumerable<User> datas;
             if (string.IsNullOrEmpty(vm.txtKeyword))
-                datas = from user in db.Users
-                        select user;
+                datas = from user in db.Users select user;
             else
-                datas = db.Users.Where(r => r.Username.Contains(vm.txtKeyword) ||
-                    r.Email.Contains(vm.txtKeyword));
-            return View(datas);
+                datas = db.Users.Where(r => r.Username.Contains(vm.txtKeyword) || r.Email.Contains(vm.txtKeyword));
+
+            int pageSize = 10; // 每頁顯示的記錄數
+            int pageNumber = page ?? 1; // 當前頁碼，默認為第1頁
+            IPagedList<User> pagedList = datas.ToPagedList(pageNumber, pageSize);
+
+            return View(pagedList);
         }
 
         public IActionResult Create()
@@ -45,21 +49,17 @@ namespace prjLookday.Controllers
                 string picName = Guid.NewGuid().ToString() + ".jpg";
                 userDb.UserPic = picName;
                 userIn.userpic.CopyTo(new FileStream(_enviro.WebRootPath + "/Images/" + picName, FileMode.Create));
-
             }
 
             userDb.Username = userIn.UserName;
             userDb.Email = userIn.Email;
             userDb.Password = userIn.Password;
             userDb.RoleId = userIn.RoleId;
-            //userDb.UserPic = userIn.UserPic;
-
 
             db.Users.Add(userDb);
             db.SaveChanges();
             return RedirectToAction("List");
         }
-
 
         public IActionResult Edit(int? id)
         {
@@ -85,41 +85,30 @@ namespace prjLookday.Controllers
                     string picName = Guid.NewGuid().ToString() + ".jpg";
                     userDb.UserPic = picName;
                     userIn.userpic.CopyTo(new FileStream(_enviro.WebRootPath + "/Images/" + picName, FileMode.Create));
-
                 }
 
                 userDb.Username = userIn.UserName;
                 userDb.Email = userIn.Email;
                 userDb.Password = userIn.Password;
                 userDb.RoleId = userIn.RoleId;
-                //userDb.UserPic = userIn.userpic;
 
                 db.SaveChanges();
             }
 
             return RedirectToAction("List");
-
         }
-
-
 
         public IActionResult Delete(int id)
         {
-            if (id != null)
+            lookdaysContext db = new lookdaysContext();
+            User u = db.Users.FirstOrDefault(x => x.UserId == id);
+            if (u != null)
             {
-                lookdaysContext db = new lookdaysContext();
-                User u = db.Users.FirstOrDefault(x => x.UserId == id);
-                if (u != null)
-                {
-                    db.Users.Remove(u);
-                    db.SaveChanges();
-                }
-
+                db.Users.Remove(u);
+                db.SaveChanges();
             }
 
             return RedirectToAction("List");
         }
-
     }
-
 }
