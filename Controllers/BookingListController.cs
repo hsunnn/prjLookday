d2e4fbe0b190f823dcbe7049ebdf1e2a53061748
2024kang <166598158+2024kang@users.noreply.github.com>
2024-06-13 22:32:31 +0800

    改為MVC

---
+++ Controllers/BookingListController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using prjLookday.Models;
+using prjLookday.ViewModels;
+
+namespace prjLookday.Controllers
+{
+    public class BookingListController : Controller
+    {
+        [Route("[controller]")]
+        public class BookingOrderController : Controller
+        {
+            private readonly lookdaysContext _context;
+
+            public BookingOrderController(lookdaysContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<IActionResult> List()
+            {
+                var bookings = await _context.Bookings
+                    .Include(x => x.User)
+                    .Include(x => x.Activity)
+                    .Include(x => x.BookingStates)
+                    .Include(x => x.Payments)
+                    .Select(b => new CBookingListViewModel
+                    {
+                        BookingId = b.BookingId,
+                        BookingDate = (DateTime)b.BookingDate,
+                        UserName = b.User.Username,
+                        ActivityName = b.Activity.Name,
+                        Price = b.Price,
+                        bookingStatus = b.BookingStates.States,
+                        member = (int)b.Member,
+                    })
+                    .ToListAsync();
+
+                return View(bookings);
+            }
+        }
+    }
+}
