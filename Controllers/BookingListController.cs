0c0c459877aa4e201e00dbb40bfe6aea35b9e3b2
2024kang <kang08050309@gmail.com>
2024-06-14 10:08:53 +0800

    修改為MVC

--- Controllers/BookingListController.cs
+++ Controllers/BookingListController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using prjLookday.Models;
 using prjLookday.ViewModels;
 
@@ -8,16 +12,45 @@ namespace prjLookday.Controllers
     [Route("[controller]")]
     public class BookingListController : Controller
     {
-        
-         private readonly lookdaysContext _context;
 
-            public BookingListController(lookdaysContext context)
+        private readonly lookdaysContext _context;
+
+        public BookingListController(lookdaysContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet("SimpleList")]
+        public async Task<IActionResult> SimpleList()
+        {
+            try
             {
-                _context = context;
+                var bookings = await _context.Bookings.Take(10).Select(b => new
+                {
+                    b.BookingId,
+                    b.BookingDate
+                }).ToListAsync();
+                return View(bookings);
             }
-            [HttpGet("List")]
-            public async Task<IActionResult> List()
+            catch (Exception ex)
             {
+                Console.WriteLine($"Exception Message:{ex.Message}");
+                Console.WriteLine($"Stack Trace:{ex.StackTrace}");
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine($"Inner Exception: {ex.InnerException.GetType().FullName}");
+                    Console.WriteLine($"Inner Exception Message: {ex.InnerException.Message}");
+                    Console.WriteLine($"Inner Exception Stack Trace: {ex.InnerException.StackTrace}");
+                }
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+
+        [HttpGet("List")]
+        public async Task<IActionResult> List()
+        {
                 var bookings = await _context.Bookings
                     .Include(x => x.User)
                     .Include(x => x.Activity)
@@ -29,7 +62,7 @@ namespace prjLookday.Controllers
                         BookingDate = (DateTime)b.BookingDate,
                         UserName = b.User.Username,
                         ActivityName = b.Activity.Name,
-                        Price = b.Price,
+                        Price = $"${b.Price:F2}",
                         bookingStatus = b.BookingStates.States,
                         member = (int)b.Member,
                     })
@@ -37,6 +70,8 @@ namespace prjLookday.Controllers
 
                 return View(bookings);
             }
+      
         }
-    
-}
+
+    }
+
