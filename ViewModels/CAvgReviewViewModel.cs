56e9c90c0f1fa3a42b17c0d9a3ede10cd5ac0687
Hsun <pp0932451127@gmail.com>
2024-06-22 16:36:14 +0800

    新增 review average viewmodel

---
+++ ViewModels/CAvgReviewViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace prjLookday.ViewModels
+{
+    public class CAvgReviewViewModel
+    {
+        public int ActivityID { get; set; }
+        public string Name { get; set; }
+        public byte[] Photo { get; set; }
+        public string Description { get; set; }
+        public double AverageRating { get; set; }
+    }
+}
